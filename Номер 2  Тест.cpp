813ea05f5e6d87def9f136969fcc82ab9301c53b
Factor1694 <factor1694@mail.ru>
2016-11-28 23:11:07 +0300

    Add files via upload

---
+++ Номер 2  Тест.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main ()
+{
+ float a, b, c;
+    cout<<"a="; cin >>a;
+    cout<<"b="; cin >>b;
+    c=a*b;
+    cout <<"chislo" << c << endl;
+    return 0;
+}
