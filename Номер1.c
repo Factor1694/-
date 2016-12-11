4ab950064541d2c14f816a93c152137f390baec1
Factor1694 <factor1694@mail.ru>
2016-12-11 03:30:41 +0300

    Домашка_Прога

---
+++ Номер1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int calcN(int n){
+	int an = n;
+	if (an > 0)
+		an *= 2;
+	else if (an < 0)
+		an -= 3;
+	else
+		an = 10;
+	return an;
+}
+
+int main (int argc, char *argv[])
+{
+	int n;
+	printf ("Please, enter n: ");
+	scanf("%d", &n);
+	n = calcN(n);
+
+	printf ("Result: %d", n);
+	return 0;
+}
