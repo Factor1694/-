afadede78d696ea99c0b59ad1d943590c6521d24
Factor1694 <factor1694@mail.ru>
2016-12-11 11:54:53 +0300

    Домашка_Прога

---
+++ Номер12.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int max_row(int N, int M){
+    int   i,  j,  k;
+    float S,  MaxSum;
+    float **a ;
+    k=0;
+    S=0;
+    MaxSum=0;
+     a=(float **) malloc(N*sizeof(float*));  // выделение пам€ти
+    if (a == NULL)
+        {
+            printf ("Memory allocation error.");
+            return 0;
+    }
+    for  (i=0; i<N; i++){
+        a[i]=(float *) malloc(M*sizeof(float));
+    }
+    printf("Vvedite matrichie elementi.\n"); // ввод элементов матрицы
+    for  (i=0; i<N; i++){
+        S=0;
+        for(j=0; j<M; j++) {    // последовательно вычисл€ютс€ суммы строк
+            scanf("%f", &a[i][j]);
+            S+=a[i][j];
+        }
+        if (S > MaxSum) {  // находим номер строки  с максимальной суммой
+            MaxSum=S;
+            k=i;
+        }
+    }
+    printf("Nomer stroki s naibolshei summoi elementov: %d", k+1);
+}
+
+int main(int argc, char *argv[]){
+    int N, M, x;
+    printf("Vvedite razmer matrici\n"); // задаем размер матрицы
+    do{
+        scanf("%d" "%d", &N, &M);
+        if (N<=0  ||  M<=0) printf("Error. Try again.\n");
+    } while (N<=0   ||   M<=0);
+    x = max_row(N, M);
+    return 0;
+}
+
