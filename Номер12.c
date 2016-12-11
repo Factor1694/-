37e28f9be14923c413cf014702f3d7bc02f4e75f
Factor1694 <factor1694@mail.ru>
2016-12-11 14:13:27 +0300

    Update Номер12.c

--- Номер12.c
+++ Номер12.c
@@ -8,7 +8,7 @@ int max_row(int N, int M){
     k=0;
     S=0;
     MaxSum=0;
-     a=(float **) malloc(N*sizeof(float*));  // выделение пам€ти
+     a=(float **) malloc(N*sizeof(float*));  // THIS IS VIDILENIE PAMYATI!
     if (a == NULL)
         {
             printf ("Memory allocation error.");
@@ -17,14 +17,14 @@ int max_row(int N, int M){
     for  (i=0; i<N; i++){
         a[i]=(float *) malloc(M*sizeof(float));
     }
-    printf("Vvedite matrichie elementi.\n"); // ввод элементов матрицы
+    printf("Vvedite matrichie elementi.\n"); // A ETO VVOD OF ELEMENTS OF MATRIX!
     for  (i=0; i<N; i++){
         S=0;
-        for(j=0; j<M; j++) {    // последовательно вычисл€ютс€ суммы строк
+        for(j=0; j<M; j++) {    // POSLEDOVATELNOE VICHISLENIE STROK
             scanf("%f", &a[i][j]);
             S+=a[i][j];
         }
-        if (S > MaxSum) {  // находим номер строки  с максимальной суммой
+        if (S > MaxSum) {  // NAHOZDENIE NOMERA STROKI S MAKSIMALNOI SUMMOI
             MaxSum=S;
             k=i;
         }
@@ -34,7 +34,7 @@ int max_row(int N, int M){
 
 int main(int argc, char *argv[]){
     int N, M, x;
-    printf("Vvedite razmer matrici\n"); // задаем размер матрицы
+    printf("Vvedite razmer matrici\n"); // ZADAEM RAZMERI MATRIX
     do{
         scanf("%d" "%d", &N, &M);
         if (N<=0  ||  M<=0) printf("Error. Try again.\n");
